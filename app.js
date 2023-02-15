10fe3768048dcb05b7aff0fb526fa321252072fd
Nimshi02 <nimshisulakshana@gmail.com>
2023-02-15 14:57:19 +0530

    Changing app.js

--- app.js
+++ app.js
@@ -9,37 +9,38 @@ var port = 9000;
 app.post('/validate', function(req, res) {
   var username=req.body.username;
   var email=req.body.email;
-var testjson={[
-  {
+  var test2={
     "row_id": 2,
     "column": "employee_name",
     "severity": "warning",
     "message": "Employee no longer works for company."
-  },
-  {
+  };
+  var test3={
     "row_id": 2,
     "column": "employee_end_date",
     "severity": "error",
     "message": "Employee gave notice on this date.",
     "suggestion": "04/15/2020"
-  },
-  {
+  };
+  var test4={
     "row_id": 3,
     "column": "employee_name",
     "severity": "error",
     "message": "Employee's name has changed",
     "suggestion": "Johnny Appleseed"
   }
-]};
-  if(username==email)
-{
-	console.log("True");
-	res.send(null);
-}
-else
-{
-	res.send(testjson);
-}
+test2.test=new Array();
+resut2.test.push(test3)
+ // if(username==email)
+//{
+//	console.log("True");
+//	res.send(null);
+//}
+//else
+//{
+//	res.send(testjson);
+//}
+res.send(JSON.stringify(result));
 });
 app.get('/',function(req,res){
 res.send("Hello from server");
