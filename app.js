5bdf3caff66fed04f74f04b5ca3ff96b58b13ad1
Nimshi02 <nimshisulakshana@gmail.com>
2023-02-15 12:00:19 +0530

    making the changes to app.js

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ app.use(bodyParser.json())
 
 var port = 9000;
 
-//app.post('/', function(req, res) {
-  //var username=req.body.username;
-//  var email=req.body.email;
-//  if(username==email)
-//{
-//	console.log("True");
-//	res.send(null);
-//}
-//else
-//{
-//	res.send(username);
-//}
-//});
+app.post('/validate', function(req, res) {
+  var username=req.body.username;
+  var email=req.body.email;
+  if(username==email)
+{
+	console.log("True");
+	res.send(null);
+}
+else
+{
+	res.send(username);
+}
+});
 app.get('/',function(req,res){
 res.send("Hello from server");
 });
