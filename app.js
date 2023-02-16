f78ba157e8402010e8a54727fe9661284f2124a4
Nimshi02 <nimshisulakshana@gmail.com>
2023-02-16 09:31:46 +0530

    Changing the app.js file

--- app.js
+++ app.js
@@ -5,56 +5,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 var port = 9000;
-var variable=null;
-app.post('/validate', function(req, res) {
-  var postBody=req.body;
+var variable = null;
+app.post('/validate', function (req, res) {
+  var postBody = req.body;
 
-//retrive the required values
-// var birthdate=postBody.rows[0].values.
+  //Creating a new array variable
+  var final = new Array();
+
+  //Loop through the postBody array
+  for (var i = 0; i < postBody.length; i++) {
+    //Retrive the required values
+    var age = postBody.rows[i].values.age;
+
+    //Condition to check
+    if (age != 20) {
+      var errorMsg={
+        "row_id": i,
+        "column": "age",
+        "severity": "error",
+        "message": "Required age does not match."
+      };
+      final.push(errorMsg);
+    }
+
+  }
+  res.send(JSON.stringify(final));
 
 
-  var final=new Array();
-  var test2={
-    "row_id": 2,
-    "column": "email",
-    "severity": "warning",
-    "message": "Employee no longer works for company."
-  };
-  var test3={
-    "row_id": 2,
-    "column": "birthdate",
-    "severity": "error",
-    "message": "Employee birthday is different.",
-    "suggestion": "04/05/2003"
-  };
-  var test4={
-    "row_id": 3,
-    "column": "age",
-    "severity": "error",
-    "message": "Ege is wrong",
-    "suggestion": "20"
-  };
-final.push(test2);
-final.push(test3);
-final.push(test4);
- // if(username==email)
-//{
-//	console.log("True");
-//	res.send(null);
-//}
-//else
-//{
-//	res.send(testjson);
-//}a
-variable=JSON.stringify(postBody);
-console.log(JSON.stringify(postBody));
-res.send(JSON.stringify(final));
 });
-app.get('/',function(req,res){
-res.send("Hello from server");
-});
-console.log(JSON.stringify(variable));
-console.log("I am in outside of methods");
+
+
 // start the server
 app.listen(port);
 console.log('Server started! At http://localhost:' + port);
